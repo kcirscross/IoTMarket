99696d689f482aa9154c89c61a1d5a2e92ccc332
Pham Duy <pdduypham@pdduypham.local>
2023-12-12 23:05:22 +0700

    Add Typescript, Lint and Prettier settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    env: {
+        'jest/globals': true,
+    },
+    root: true,
+    extends: ['@react-native-community'],
+    plugins: ['jest'],
+    rules: {
+        semi: 0,
+        'object-curly-spacing': ['error', 'always'],
+        'array-bracket-spacing': ['error', 'never'],
+        'react/require-default-props': ['off'],
+        'react/default-props-match-prop-types': ['error'],
+        'react/sort-prop-types': ['error'],
+        'react-hooks/exhaustive-deps': 'off',
+        'no-extra-semi': ['off'],
+        'react/no-unstable-nested-components': ['off'],
+    },
+    settings: {
+        'import/resolver': {
+            'babel-module': {},
+        },
+    },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  tabWidth: 4,
-  semi: false,
+    arrowParens: 'avoid',
+    bracketSameLine: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    endOfLine: 'auto',
+    printWidth: 80,
+    useTabs: false,
 };
